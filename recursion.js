550c66f7ad4dd8ff6c44d3f681837212955002b7
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2018-10-28 12:09:42 +0300

    feat: recursion solution
    add recursion solution with test

---
+++ recursion.js
@@ -0,0 +1,22 @@
+let arr = [];
+
+module.exports = function recursion(a) {
+    treeToArray(a, 0);
+    return arr;
+}
+
+function treeToArray(a, level) {
+    if (arr.length === level) {
+        arr.push([]);
+    }
+
+    Object.keys(a).forEach(key => {
+        if (key === 'value') {
+            arr[level].push(a[key]);
+        }
+        if (key === 'left' || key === 'right') {
+            treeToArray(a[key], level + 1);
+        }
+    })
+}
+
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const sumOfOther = require('./sum-of-other.js');
 const make = require('./make.js');
+const recursion = require('./recursion.js');
 
 describe('sum of other', () => {
     it('1', () => {
@@ -24,5 +25,11 @@ describe('make', () => {
         const result = make(5, 8)(8, 5, 1)(9)(mul);
         assert.equal(result, 14400);
     });
-  
+});
+
+describe('recursion', () => {
+    it('1', () => {
+        const result = recursion({"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}});
+        assert.deepEqual(result, [ [ 100 ], [ 90, 120 ], [ 70, 99, 110, 130 ] ]);
+    });
 });
